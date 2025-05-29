567b21e666ae1a51506241e45d3eaacdc457316f
Abraham Varghese <kochujosy2003@gmail.com>
2025-05-29 16:34:34 +0530

    Add root URL view to prevent 404 on /

--- bitespeed/urls.py
+++ bitespeed/urls.py
@@ -1,7 +1,13 @@
+# bitespeed/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def root_view(request):
+    return HttpResponse("Welcome to Bitespeed API!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contacts.urls')),
+    path('identify/', include('contacts.urls')),  # your existing app routes
+    path('', root_view),  # Add this line to handle root URL
 ]
